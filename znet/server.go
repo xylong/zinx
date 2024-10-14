6bfa2c5664eb996dcb7a5cb7ad71bee61c90a756
xyl <416319808@qq.com>
2024-10-14 23:48:06 +0800

    架构

--- znet/server.go
+++ znet/server.go
@@ -30,46 +30,50 @@ func NewServer(name string) ziface.IServer {
 func (s *Server) Start() {
 	fmt.Printf("start tcp on %s:%d\n", s.IP, s.Port)
 
-	// 1.获取tcp连接地址
-	addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
-	if err != nil {
-		fmt.Println("resolve tcp addr error:" + err.Error())
-		return
-	}
-
-	// 2.监听服务器地址
-	listener, err := net.ListenTCP(s.IPVersion, addr)
-	if err != nil {
-		fmt.Println("listen tcp addr error:" + err.Error())
-		return
-	}
-
-	// 3.阻塞等待客户端连接，处理客户端业务(读写)
-	for {
-		conn, err := listener.AcceptTCP()
+	go func() {
+		// 1.获取tcp连接地址
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
 		if err != nil {
-			fmt.Println("accept tcp error" + err.Error())
-			continue
+			fmt.Println("resolve tcp addr error:" + err.Error())
+			return
 		}
 
-		go func() {
-			for {
-				// 读数据
-				buf := make([]byte, 512)
-				count, err := conn.Read(buf)
-				if err != nil {
-					fmt.Println("receive buf error:" + err.Error())
-					continue
-				}
+		// 2.监听服务器地址
+		listener, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("listen tcp addr error:" + err.Error())
+			return
+		}
 
-				// 返回数据
-				if _, err = conn.Write(buf[:count]); err != nil {
-					fmt.Println("write buf error:" + err.Error())
-					continue
-				}
+		// 3.阻塞等待客户端连接，处理客户端业务(读写)
+		for {
+			conn, err := listener.AcceptTCP()
+			if err != nil {
+				fmt.Println("accept tcp error" + err.Error())
+				continue
 			}
-		}()
-	}
+
+			go func() {
+				for {
+					// 读数据
+					buf := make([]byte, 512)
+					count, err := conn.Read(buf)
+					if err != nil {
+						fmt.Println("receive buf error:" + err.Error())
+						continue
+					}
+
+					// 返回数据
+					if _, err = conn.Write(buf[:count]); err != nil {
+						fmt.Println("write buf error:" + err.Error())
+						continue
+					}
+				}
+			}()
+		}
+	}()
+
+	// todo 扩展
 }
 
 func (s *Server) Stop() {
@@ -77,7 +81,7 @@ func (s *Server) Stop() {
 }
 
 func (s *Server) Run() {
-	go s.Start()
+	s.Start()
 
 	select {}
 }
